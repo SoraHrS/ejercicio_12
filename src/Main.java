a28ba0b78abb57eedde440bd537190e86558cbbc
SoraHrS <soherreras@unal.edu.co>
2024-12-02 16:07:25 -0500

    se realizó el ejercicio 12

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String nombre;
+        double horasTrabajadas, valorHoraNormal, horasExtrasTrabajadas, hET8, salario;
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.print("Ingrese el nombre del trabajador: ");
+        nombre = sc.nextLine();
+        System.out.print("Ingrese el número de horas trabajadas: ");
+        horasTrabajadas = sc.nextDouble();
+        System.out.print("Ingrese el valor de la hora normal trabajada: ");
+        valorHoraNormal = sc.nextDouble();
+
+        if (horasTrabajadas>40){
+            horasExtrasTrabajadas = horasTrabajadas - 40;
+            if (horasExtrasTrabajadas>8){
+                hET8 = horasExtrasTrabajadas - 8;
+                salario = 40 * valorHoraNormal + 16 * valorHoraNormal + hET8 * 3 * valorHoraNormal;
+            }else{
+                salario = 40 * valorHoraNormal + horasExtrasTrabajadas * 2 * valorHoraNormal;
+            }
+        }else{
+            salario = horasTrabajadas * valorHoraNormal;
+        }
+    System.out.println("El trabajador " + nombre + " devengó: $ "+ salario);
     }
 }
